a162d648aa751f616635806cca373a545bb6d039
Jiří Klíč <jklic@acmark.com>
2018-11-20 15:04:49 +0100

    asd

--- SharePointProject2/Scripts/CreateFolder.ps1
+++ SharePointProject2/Scripts/CreateFolder.ps1
@@ -2,7 +2,7 @@
 git fetch
 $status = git status
 
-if($status -eq "*Your branch is up to date*" -and $status -eq "*nothing to commit*")
+if($status -like "*Your branch is up to date*" -and $status -like "*nothing to commit*")
 {
     write-host Update
 }

16f499cbc13d5753c154c3570612585351ec0050
Jiří Klíč <jklic@acmark.com>
2018-11-21 11:59:24 +0100

    asd

--- SharePointProject2/Scripts/CreateFolder.ps1
+++ SharePointProject2/Scripts/CreateFolder.ps1
@@ -4,21 +4,49 @@ $status = git status
 
 if($status -like "*Your branch is up to date*" -and $status -like "*nothing to commit*")
 {
-	$gitlog = git log --pretty=oneline --decorate=short
+    $gitlog = git log --pretty=oneline --decorate=short
+    $lastCommit = ""
 	$lastCommit = $($gitlog -split "`r`n")[0]
 
 	$oneTagOnly = "(\(tag: [0-9.]+),"
 	$moreTags = "(\(tag: [0-9.]+\))"
 
-	if($lastCommit -notlike "tag: ")
+	if($lastCommit -notlike "*tag: *")
 	{
-		exit 2
+		#exit 2
 		ForEach ($line in $($gitlog -split "`r`n"))
 		{
 			if($line -like "*tag: *")
 			{
+				$tag = ""
 				$tag = [regex]::Matches($line, $oneTagOnly)
+                if($tag.Count -eq 0)
+                {
+                    $tag = [regex]::Matches($line, $moreTags)
+                    if($tag.Count -eq 0)
+                    {
+                        continue
+                    }
+                }
+
+                $tag = $tag[0].Value
+                
+                if ($tag -ne "" -and $tag -ne $null)
+                {
+                    $tag = $tag -replace "\(tag: ",""
+                    $tag = $tag -replace ",",""
+                    $tag = $tag -replace "\)",""
+                    break
+			    }
 			}
+		}
+		if($tag -eq "" -or $tag -eq $null)
+		{
+			exit 3
+		}
+		else
+		{
+            git tag $tag
 		}
 	}
 }
---
+++ SharePointProject2/Version.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointProject2
+{
+    public class Version
+    {
+        public string Verze { get; } = "1.0";
+    }
+}
